34c9324cb365807ef79e8ae43dc36b5d7cb662b6
Shinji Yamada <dotneet@gmail.com>
2023-07-12 22:13:21 +0900

    update lightning

--- inference_test.py
+++ inference_test.py
@@ -1,12 +1,26 @@
 from bttr.lit_bttr import LitBTTR
 from PIL import Image
 from torchvision.transforms import ToTensor
+import time
+import torch
 
+device = torch.device("cpu")
 ckpt = './pretrained-2014.ckpt'
+# ckpt = './self.ckpt'
+
+start = time.time()
 img_path = './example/18_em_1.bmp'
-model = LitBTTR.load_from_checkpoint(ckpt)
+model = LitBTTR.load_from_checkpoint(ckpt, map_location=device)
+print(f"load weight: {time.time() - start}")
+
+start = time.time()
 img = Image.open(img_path)
-img = ToTensor()(img)
-hyp = model.beam_search(img)
-print(hyp)
+img = ToTensor()(img).to(device)
+print(f"ToTensor: {time.time() - start}")
+
+for i in range(0,1):
+    hyp = model.beam_search(img)
+    print(hyp)
+    print(time.time() - start)
+
 
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from bttr.datamodule import CROHMEDatamodule
 from bttr.lit_bttr import LitBTTR
 
 def main():
-    cli = LightningCLI(LitBTTR, CROHMEDatamodule)
+    cli = LightningCLI(LitBTTR, CROHMEDatamodule, save_config_kwargs={"overwrite": True})
 
 if __name__ == '__main__':
     main()
